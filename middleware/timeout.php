72483e88c411c25dce7bb8af97079f1917a7735a
Avaye <Avaye@123.com>
2020-06-27 17:32:50 -0400

    Finished the timeout

--- middleware/timeout.php
+++ middleware/timeout.php
@@ -17,23 +17,18 @@ if($credentials == "This key has been tampered with or is out of date." || $cred
 $client = new MongoDB\Client('mongodb+srv://dbrunner:AWsAcctcHfb1g8FG@cluster0-vixlf.mongodb.net/hackathon?retryWrites=true&w=majority');
 $collection = $client->hackathon->userdata;
 
-$document = $collection->(["username" => $credentials->username]);
-$books = $document->books;
-foreach ($books as $bk) {
-    if($bk->ISBN == $isbn) {
-        $bk->copies--;
-    }
-}
-$collection->updateOne(["username" => $credentials->username], ['$set' => ['books' => $books]]);
 
 $collection = $client->hackathon->traderoom;
-$collection->insertOne([
-    "seller" => $credentials->username,
-    "isbn" => $isbn,
-    "zipcode" => $credentials->zip,
-    "tradeMade" => false,
-    "dateCreated" => time(),
-    "queue" => []
-]);
-echo "Book added sucessfully";
+$cursor = $collection->find(["username" => $credentials->username]);
+
+$returnArray = array();
+
+foreach ($cursor as $document){
+    if count($document->queue) >= 1{
+        array_push($returnArray, $document);
+    }
+}
+
+return $returnArray
+
 ?>
