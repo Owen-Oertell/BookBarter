3a89a9c7f96381a4620aece30cc4462a7d7a2e65
Owen Oertell <owen@oertell.net>
2020-06-27 22:09:25 -0400

    Adding search features

--- middleware/search.php
+++ middleware/search.php
@@ -17,18 +17,16 @@ if($credentials == "This key has been tampered with or is out of date." || $cred
 
 $client = new MongoDB\Client('mongodb+srv://dbrunner:AWsAcctcHfb1g8FG@cluster0-vixlf.mongodb.net/hackathon?retryWrites=true&w=majority');
 
-$collection = $client->hackathon->userdata;
-$collection->updateOne(["username" => $credentials->username], ['$set' => ['books' => $bookArray]]);
-
 $newCollection = $client->hackathon->traderoom;
-$cursor = $collection->find([]);
+$cursor = $newCollection->find([]);
 $viableQuery = [];
 
 foreach($cursor as $document){
-    $zipcode1 = $document->zip;
+    $zipcode1 = $document->zipcode;
     $zipcode2 = $credentials->zip;
     $difference = include "zipCompare.php";
-    if($difference > $distance){
+    var_dump($difference);
+    if(floatval($difference) > floatval($distance)){
         continue;
     }
     if(strstr($query,$document->author) || strstr($query,$document->title)){
