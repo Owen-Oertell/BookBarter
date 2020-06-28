b6a1c43369549c8e043b1100d8bd06d8baa3620f
Owen Oertell <owen@oertell.net>
2020-06-27 22:48:28 -0400

    Adding all angular components

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SlidingComponent } from './sliding/sliding.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './pages/search/search.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlidingComponent,
+    FooterComponent,
+    SearchComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
