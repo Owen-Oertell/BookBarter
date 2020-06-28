b01bad3adf72080ded7f6c171faf026b34087f98
Avaye <Avaye@123.com>
2020-06-27 22:07:17 -0400

    Fixed zipcompare to make it actually output a float

--- middleware/zipCompare.php
+++ middleware/zipCompare.php
@@ -1,5 +1,6 @@
 <?php
     $query = "https://www.zipcodeapi.com/rest/Z7XXXSNdddvFoqVgXVYpd8WuktqzPbWcEdfxrQcvmjjMZGKNSar1L0NBwSUn9Bq5/distance.json/$zipcode1/$zipcode2/miles";
     $data = file_get_contents($query);
-    return $data[12] . $data[13] . $data[14] . $data[15] . $data[16]
+    $data = json_decode($data, true);
+    return $data["distance"];
 ?>
